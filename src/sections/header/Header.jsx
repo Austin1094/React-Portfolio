7d482f757a569836260c4e5699ce893e6c27850e
Austin1094 <Austin.Adams1094@gmail.com>
2023-07-12 23:09:18 -0400

    Customizing header files

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import data from './data'
 import './header.css'
 
 const Header = () => {
     return (
-        <header id="header"><h2>Header</h2></header>
+        <header id="header">
+            <div className="container header__container">
+                <div className="header__profile">
+                    <img src="" alt="" />
+                </div>
+                <h3>Austin Adams</h3>
+                <p>Some text will go here later once I think of something better to write</p>
+                <div className="header__cta">
+                    <a href="#contact">Reach Out To Me</a>
+                    <a href="#portfolio">Projects</a>
+                </div>
+                <div className="header__socials">
+                    {
+                        data.map(item => <a key={item.id} href={item.link} target="_blank" rel="noopener noreferrer">{item.icon}</a>)
+                    }
+                </div>
+            </div>
+        </header>
     )
 }
 
---
+++ src/sections/header/data.js
@@ -0,0 +1,9 @@
+import { AiFillGithub } from 'react-icons/ai'
+import { AiFillLinkedin } from 'react-icons/ai'
+
+const data = [
+    { id: 1, link: 'https://github.com/Austin1094', icon: <AiFillGithub /> },
+    { id: 2, link: 'https://www.linkedin.com/in/austin-adams-480620191/', icon: <AiFillLinkedin /> },
+]
+
+export default data
