e4612ae0a18f9b3a36d1a142be77c1577aef2975
Austin1094 <Austin.Adams1094@gmail.com>
2023-07-16 22:12:15 -0400

    More being done with about

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './about.css'
+import data from './data'
+import Card from '../../components/card'
 
 const About = () => {
     return (
@@ -12,7 +14,17 @@ const About = () => {
                 </div>
                 <div className="about__right">
                     <h2>About Me</h2>
-                    <div className="about__cards"></div>
+                    <div className="about__cards">
+                        {
+                            data.map(item => (
+                                <Card key={item.id} className="about__card">
+                                    <span className='about__card-icon'>{item.icon}</span>
+                                    <h3>{item.title}</h3>
+                                    <small>{item.description}</small>
+                                </Card>
+                            ))
+                        }
+                    </div>
                     <p>
                         More code will be put here later once I think what I want to type.
                     </p>
---
+++ src/sections/about/data.js
@@ -0,0 +1,9 @@
+import { BsFillFileCodeFill } from "react-icons/bs";
+import { GrAchievement } from 'react-icon/gr'
+
+const data = [
+    { id: 1, icon: <BsFillFileCodeFill />, title: "Experience", description: "6 months of a Full Stack Coding Bootcamp, with additional courses from other sites" },
+    { id: 2, icon: <GrAchievement />, title: "Projects", description: "2, with a third project coming up" }
+]
+
+export default data
