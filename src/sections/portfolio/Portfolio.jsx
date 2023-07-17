138e93c65f910f581ba55ae97c1601b71074312b
Austin1094 <Austin.Adams1094@gmail.com>
2023-07-16 23:44:14 -0400

    Making a mess, but will clean it up *hopefully*

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
 import './portfolio.css'
+import Projects from './Projects'
+import ProjectsCategories from './ProjectsCategories'
+import data from './data'
+import { useState } from 'react'
 
-const portfolio = () => {
+const Portfolio = () => {
+    const [projects, setProjects] = useState(data);
+
     return (
-        <section id="portfolio"><h2>Portfolio</h2></section>
+        <section id="portfolio">
+            <h2>Projects</h2>
+            <p>Here is where you can find projects that I've worked on and also contributed in!</p>
+            <div className="container portfolio__container">
+                <ProjectsCategories />
+                <Projects projects={projects} />
+            </div>
+        </section>
     )
 }
 
-export default portfolio
+export default Portfolio
---
+++ src/sections/portfolio/data.js
@@ -0,0 +1,23 @@
+import Image1 from "/Users/austinadams/bootcamp/react-portfolio/src/assets/project1.jpg"
+import Image2 from "/Users/austinadams/bootcamp/react-portfolio/src/assets/project2.jpg"
+
+const data = [
+    {
+        id: 1,
+        image: Image1,
+        title: "Room For Activities",
+        description: "Room For Activities allows users to search for and buy tickets to an event they would like to attend. They're able to search by category, state that it's held in, the start date, and the end date. Once the requirements are filled in, the list of events within the dates entered will show, along with a button to pin where at on the map it's located. From there, the user can click the pin on the map, and it will direct them Tickermaster, where they can then buy the tickets.",
+        link: 'https://reallyawesomename.github.io/room-for-activities/',
+        github: 'https://github.com/ReallyAwesomeName/room-for-activities'
+    },
+    {
+        id: 2,
+        image: Image2,
+        title: "Achievement Unlocked",
+        description: "Achievement Unlocked allows users to connect with other passionate gamers, as they can review and talk about their favorite video games. With plans to help build on Achievement Unlocked, we hope to make it the go-to place for all gaming needs.",
+        link: 'https://thawing-cove-75959.herokuapp.com/',
+        github: 'https://github.com/Greivin13/AchievementUnlocked '
+    }
+]
+
+export default data
