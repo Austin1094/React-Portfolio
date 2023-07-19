a155c0ebdb3d96910e406bac9c9bba0a08794370
Austin1094 <Austin.Adams1094@gmail.com>
2023-07-18 21:18:06 -0400

    Fixed some styling and worked on footer

---
+++ src/sections/footer/data.js
@@ -0,0 +1,15 @@
+import { BsGithub } from 'react-icons/bs';
+import { BsLinkedin } from 'react-icons/bs';
+
+export const links = [
+    { id: 1, link: '#', title: 'Home' },
+    { id: 2, link: '#about', title: 'About' },
+    { id: 3, link: '#services', title: 'Services' },
+    { id: 4, link: '#portfolio', title: 'Portfolio' },
+    { id: 5, link: '#contact', title: 'Contact' },
+]
+
+export const socials = [
+    { id: 1, link: 'https://github.com/Austin1094', icon: <BsGithub /> },
+    { id: 2, link: 'https://www.linkedin.com/in/austin-adams-480620191', icon: <BsLinkedin /> },
+]
