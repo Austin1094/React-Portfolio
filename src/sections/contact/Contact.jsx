d6781046b76696f2892b4688cec693039ed36fc5
Austin1094 <Austin.Adams1094@gmail.com>
2023-07-18 20:45:30 -0400

    Better styling just needed for contact

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import contacts from './data'
 import './contact.css'
 
 const Contact = () => {
     return (
-        <section id="contact"><h2>Contacts</h2></section>
+        <section id="contact">
+            <h2>Contacts</h2>
+            <p>Please reach out to me from one of the following below!</p>
+            <div className="container contact__container">
+                {
+                    contacts.map(contact => <a key={contact.id} href={contact.link} target="_blank" rel="noopener noreferrer">{contact.icon}</a>)
+                }
+            </div>
+        </section>
     )
 }
 
---
+++ src/sections/contact/data.js
@@ -0,0 +1,9 @@
+import { MdEmail } from 'react-icons/md';
+import { BsLinkedin } from 'react-icons/bs';
+
+const data = [
+    { id: 1, icon: <MdEmail />, link: 'mailto:Austin.Adams1094@gmail.com' },
+    { id: 2, icon: <BsLinkedin />, link: 'www.linkedin.com/in/austin-adams-480620191' }
+]
+
+export default data;
