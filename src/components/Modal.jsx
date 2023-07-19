f0e3c83bfb7b473b7e06eaf260b364665647d01c
Austin1094 <Austin.Adams1094@gmail.com>
2023-07-18 23:09:14 -0400

    Working with modals

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Card from './Card'
 import { Fragment } from 'react'
+import { useModalContext } from '../context/modal-context'
 import './modal.css'
 
 const Modal = ({ className, children }) => {
+    const { showModal, closeModalHandler } = useModalContext();
+
     return (
         <Fragment>
             {
-                ReactDOM.createPortal(<>
-                    <section id="backdrop"></section>
+                showModal && ReactDOM.createPortal(<>
+                    <section id="backdrop" onClick={closeModalHandler}></section>
                     <Card className={className}>{children}</Card>
                 </>, document.querySelector('#overlays'))
             }
