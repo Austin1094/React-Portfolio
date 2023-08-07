80778ae0fc79ea89adf43f31451d1d8a00619043
Austin1094 <Austin.Adams1094@gmail.com>
2023-08-07 16:01:39 -0400

    Added third group project

--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -1,9 +1,10 @@
 import Image1 from "/Users/austinadams/bootcamp/react-portfolio/src/assets/project1.jpg"
 import Image2 from "/Users/austinadams/bootcamp/react-portfolio/src/assets/project2.jpg"
-import Image3 from "/Users/austinadams/bootcamp/react-portfolio/src/assets/password-generator.jpg"
-import Image4 from "/Users/austinadams/bootcamp/react-portfolio/src/assets/basketball-quiz.jpg"
-import Image5 from "/Users/austinadams/bootcamp/react-portfolio/src/assets/note-taker.jpg"
-import Image6 from "/Users/austinadams/bootcamp/react-portfolio/src/assets/work-day.jpg"
+import Image3 from "/Users/austinadams/bootcamp/react-portfolio/src/assets/body-and-mind.png"
+import Image4 from "/Users/austinadams/bootcamp/react-portfolio/src/assets/password-generator.jpg"
+import Image5 from "/Users/austinadams/bootcamp/react-portfolio/src/assets/basketball-quiz.jpg"
+import Image6 from "/Users/austinadams/bootcamp/react-portfolio/src/assets/note-taker.jpg"
+import Image7 from "/Users/austinadams/bootcamp/react-portfolio/src/assets/work-day.jpg"
 
 const data = [
     {
@@ -26,35 +27,44 @@ const data = [
     },
     {
         id: 3,
-        category: "Frontend",
+        category: "Backend",
         image: Image3,
+        title: "Body And Mind (Group Project)",
+        description: "Body And Mind is a social platform that focuses on health and fitness. Users can create posts, and even comment on others. Additionally, they're able to search for different types of workouts, categorized by specific words, giving each workout the name, type, muscle, equipment, difficulty, and even proper instructions.",
+        link: 'https://body--mind-424f753a39d8.herokuapp.com/',
+        github: 'https://github.com/EldenBear/Body_and_Mind'
+    },
+    {
+        id: 4,
+        category: "Frontend",
+        image: Image4,
         title: "Password Generator",
         description: "Password Generator allows users to generate a random password based on the length of the desired password.",
         link: 'https://austin1094.github.io/Password-Generator/',
         github: 'https://github.com/Austin1094/Password-Generator'
     },
     {
-        id: 4,
+        id: 5,
         category: "Frontend",
-        image: Image4,
+        image: Image5,
         title: "Basketball Quiz",
         description: "A short basketball quiz that gives users the chance to showcase their basketball knowledge against other users.",
         link: 'https://austin1094.github.io/created-quiz/',
         github: 'https://github.com/Austin1094/created-quiz'
     },
     {
-        id: 5,
+        id: 6,
         category: "Backend",
-        image: Image5,
+        image: Image6,
         title: "Note Taker",
         description: "Note Taker allows the user to create notes (such as reminders) and save them, so that when the page is reloaded, the note will stay put. The user also has the option to delete the notes as well.",
         link: 'https://calm-hollows-09005.herokuapp.com/',
         github: 'https://github.com/Austin1094/Note-Taker'
     },
     {
-        id: 6,
+        id: 7,
         category: "Frontend",
-        image: Image6,
+        image: Image7,
         title: "Work Day Scheduler",
         description: "Work Day Scheduler allows users to plan their day accordingly by having them fill each time slot throughout the typical nine-to-five work shift with important things to do, such as an upcoming meeting.",
         link: 'https://austin1094.github.io/New-Work-Day-Scheduler/',
